da62a33f1347f34b5adeb103f7f6cc0daceb49f3
Subhrajyoti <subha.ionic3@gmail.com>
2020-12-16 01:56:01 +0530

    Initial commit

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Feather from "react-native-vector-icons/Feather";
+
+import FavScreen from "./src/screens/Fav/Fav";
+import ListScreen from "./src/screens/List/List";
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Movies"
+          component={ListScreen}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => {
+              return <Feather name="list" color={color} size={size} />;
+            },
+          }}
+        />
+        <Tab.Screen
+          name="Favorites"
+          component={FavScreen}
+          options={{
+            tabBarIcon: ({ focused, color, size }) => {
+              return <Feather name="heart" color={color} size={size} />;
+            },
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Fav/Fav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const Fav = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Favorites</Text>
+    </View>
+  );
+};
+
+export default Fav;
