1933b916c02d42241f6a65eaf3ad9b366215594b
Subhrajyoti <subha.ionic3@gmail.com>
2021-02-19 16:51:38 +0530

    install react-query

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import "react-native-gesture-handler";
 import { StatusBar, View, Platform } from "react-native";
 import Constants from "expo-constants";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
@@ -16,6 +17,8 @@ import { MovieProvider } from "./src/reducers/movieReducer";
 const Tab = createBottomTabNavigator();
 const MovieStack = createStackNavigator();
 const FavStack = createStackNavigator();
+
+const queryClient = new QueryClient();
 
 const MovieStackScreen = () => {
   return (
@@ -52,37 +55,39 @@ export default function App() {
     return <Feather name={name} color={color} size={size} />;
   };
   return (
-    <MovieProvider>
-      <View
-        style={{
-          paddingTop: Platform.OS === "ios" ? Constants.statusBarHeight : 0,
-          flex: 1,
-        }}
-      >
-        <StatusBar
-          barStyle={Platform.OS === "ios" ? "dark-content" : "default"}
-        />
-        <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen
-              name="Movies"
-              component={MovieStackScreen}
-              options={{
-                tabBarIcon: ({ focused, color, size }) =>
-                  renderTabBarIcon(focused, color, size, "list"),
-              }}
-            />
-            <Tab.Screen
-              name="Favorites"
-              component={FavStackScreen}
-              options={{
-                tabBarIcon: ({ focused, color, size }) =>
-                  renderTabBarIcon(focused, color, size, "heart"),
-              }}
-            />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </View>
-    </MovieProvider>
+    <QueryClientProvider client={queryClient}>
+      <MovieProvider>
+        <View
+          style={{
+            paddingTop: Platform.OS === "ios" ? Constants.statusBarHeight : 0,
+            flex: 1,
+          }}
+        >
+          <StatusBar
+            barStyle={Platform.OS === "ios" ? "dark-content" : "default"}
+          />
+          <NavigationContainer>
+            <Tab.Navigator>
+              <Tab.Screen
+                name="Movies"
+                component={MovieStackScreen}
+                options={{
+                  tabBarIcon: ({ focused, color, size }) =>
+                    renderTabBarIcon(focused, color, size, "list"),
+                }}
+              />
+              <Tab.Screen
+                name="Favorites"
+                component={FavStackScreen}
+                options={{
+                  tabBarIcon: ({ focused, color, size }) =>
+                    renderTabBarIcon(focused, color, size, "heart"),
+                }}
+              />
+            </Tab.Navigator>
+          </NavigationContainer>
+        </View>
+      </MovieProvider>
+    </QueryClientProvider>
   );
 }
